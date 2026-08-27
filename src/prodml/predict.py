c6776cda674f46704ac81673bd5d63a49ce85f2e
Mariam <mariamayman292003@gmail.com>
2026-08-27 16:49:18 +0300

    feat: implement structured JSON logging and replace print statements

--- src/prodml/predict.py
+++ src/prodml/predict.py
@@ -1,26 +1,71 @@
+from numpy import tri
+from .logging_conf import setup_logging
 from .config import Config
 import pickle
-
-def load_model() -> tuple:
-    """Loads the trained model from disk."""
-    with open(Config.model_path, 'rb') as f_in:
-        dv, model = pickle.load(f_in)
-    return dv, model
+import time 
+import logging 
+from functools import wraps
 
 
-def predict(features: dict) -> float:
-    """Makes a prediction using the trained model."""
-    dv, model = load_model()
-    X = dv.transform([features])
-    prediction = model.predict(X)
-    return prediction[0]
+logger = logging.getLogger(__name__)
+
+def timed(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result= func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.info(f"Function '{func.__name__}' executed in {elapsed_time:.4f} seconds")
+        return result
+    return wrapper
+
+class duration_predictor:
+    """Class for predicting trip durations using a pre-trained model"""
+    def __init__(self):
+        self.model = None
+        self.dv = None
+        self.is_loaded = False
+
+    def load_model(self):
+
+        if not self.is_loaded:
+            logger.info(f"Loading model from {Config.model_path}")
+            with open(Config.model_path, 'rb') as f:
+                self.dv, self.model = pickle.load(f)
+            self.is_loaded = True
+            logger.info("Model loaded successfully")
+        else:
+            logger.info("Model is already loaded")
+
+    @timed
+    def predict_trip(self,trip_info: dict) -> float:
+        if not self.is_loaded:
+            self.load_model()
+
+        trip_info['PU_DO'] = str(trip_info['PULocationID']) + '_' + str(trip_info['DOLocationID'])
+        X = self.dv.transform([trip_info])
+
+        predicted_duration = self.model.predict(X)[0]
+        logger.info(f"Predicted duration for trip {trip_info}: {predicted_duration:.2f} minutes")
+        return predicted_duration
+
+    @timed
+    def predict_trips(self, trips_info: list[dict]) -> list[float]:
+        if not self.is_loaded:
+            self.load_model()
+
+        for trip in trips_info:
+            trip['PU_DO'] = str(trip['PULocationID']) + '_' + str(trip['DOLocationID'])
+        
+        X = self.dv.transform(trips_info)
+        predicted_durations = self.model.predict(X)
+        logger.info(f"Predicted durations for trips: {predicted_durations.tolist()}")
+        return predicted_durations.tolist()
 
 if __name__ == "__main__":
-    sample_trip = {
-        "PULocationID": 43,
-        "DOLocationID": 231,
-        "trip_distance": 2.5
-    }
-    
-    predicted_duration = predict(sample_trip)
-    print(f"Predicted trip duration: {predicted_duration:.2f} minutes")
+    setup_logging()
+
+
+   
--- src/prodml/train.py
+++ src/prodml/train.py
@@ -1,3 +1,5 @@
+import logging
+from .logging_conf import setup_logging
 from .config import Config
 from .data import load_data, split_data
 from .features import process_data, process_data
@@ -8,16 +10,19 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-
+logger = logging.getLogger(__name__)
 
 def train_model() -> None:
     """Trains the baseline model and saves it to disk"""
 
     df= load_data()
-    
+    logger.info(f"Loaded data from {Config.data_path} with shape {df.shape}")
+
     df = process_data(df)
+    logger.info(f"Processed data with shape {df.shape}")
 
     df_train, df_val = split_data(df)
+    logger.info(f"Split data into train ({df_train.shape}) and validation ({df_val.shape}) sets")
 
     categorical = ['PU_DO']
     numerical = ['trip_distance']
@@ -28,6 +33,8 @@ def train_model() -> None:
     
     y_train = df_train[target].values
     y_val = df_val[target].values
+
+    logger.info("Training the model...")
 
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts)
@@ -40,13 +47,14 @@ def train_model() -> None:
     mae = mean_absolute_error(y_val, y_pred)
     rmse = np.sqrt(mean_squared_error(y_val, y_pred))
     
-    print(f"Validation MAE: {mae:.5f}")
-    print(f"Validation RMSE: {rmse:.5f}")
+    logger.info(f"Validation MAE: {mae:.5f}")
+    logger.info(f"Validation RMSE: {rmse:.5f}")
     
-    print(f"Saving model to {Config.model_path}")
+    logger.info(f"Saving model to {Config.model_path}")
     with open(Config.model_path, 'wb') as f:
         pickle.dump((dv, lr), f)
         
 
 if __name__ == "__main__":
+    setup_logging()
     train_model()
